dae68af27c297d98bf9cf7d826d9ce501c98cdc7
kpearson16 <122332523+kpearson16@users.noreply.github.com>
2023-02-17 09:47:31 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,420 @@
+let menu = {
+    bevs:{
+        bev1: {
+            name: "Whiskey Sour",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi8kL4CRblmQlvbJKhgiw1JeKXWnzGNZtlag&usqp=CAU",
+            desc: "This cocktail is full of bourbon and fresh lemon, sweetened with maple syrup and includes a hint of cinnamon",
+            price: "14",
+        },
+
+        bev2: {
+            name:"Negroni",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScuK1YHi2K4W1NYsf6_XAWAEOtXkr6pr2tMg&usqp=CAU",
+            desc: "Negroni is an Italian cocktail, made of one part gin, one part vermouth rosso (red, semi-sweet) and one part Campari, garnished with orange peel.",
+            price: "14",
+    },
+        bev3: {
+            name:"Long Island Iced Tea",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvCIwyjzfUI6mXeARTKq9yoTAiZ_p8FRZpiA&usqp=CAU",
+            desc: "An unusual blend of vodka, gin, tequila, rum and orange liqueur with cola, the Long Island Iced Tea flavours of vanilla and lightly tannic wood make it taste a little like tea, but in fact they come from a mix of quality aged spirits.",
+            price: "16",
+    },
+        bev4: {
+            name: "Margarita",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBM-jIUI2w2LZYoCmsYP-v780f8kIKHR9BSQ&usqp=CAU",
+            desc: "A house lime margarita made from tequila, lime juice and triple sec. Frozen or on the rocks.",
+            price: "12",
+    },
+        bev5: {
+            name: "Moscow Mule",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2mB2iYOAoAuJdQprFBXoT9xj_q6lhLWkCBQ&usqp=CAU",
+            desc: "Moscow mules are delightfully fizzy drinks made with fiery ginger beer, vodka and fresh lime. They strike that elusive balance between spicy, sweet and strong.",
+            price: "14",
+    }
+},
+    apps: {
+        app1: {
+            name:"THE LEWIS",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlQTnW63gcmCD461K5vde_ZGIkd2mF15Ol6A&usqp=CAU",
+            desc: "Spinach And Artichoke Dip Housemade Tortilla Chips",
+            price: "11"
+    },
+        app2: {
+            name:"BROOKLYN WINGS 2.0",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtdLvckapuds9h3NWZS7wLDv3FaOrDFDLREQ&usqp=CAU",
+            desc:"Sweet And Spicy Wings, Alabama White Drizzle",
+            price: "11"
+    },
+        app3: {
+            name: "SMOKED CHICKEN AND SAUSAGE GUMBO",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQpbIlPbDkhseqKZ7Hd6D8Gl6YQaotDqocmg&usqp=CAU",
+            desc: "Carolina Gold Rice, Scallions", 
+            price: "10"
+    },
+
+
+        app4: {
+            name: "OVEN BAKED GRITS",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-jiIWKbbWNsf_K9V_JosPdmivgfIgRHc9HA&usqp=CAU",
+            desc: "Simmered Black Angus Beef, Parmesan, Arugula Salad",
+            price: "13",
+    },
+
+        app5: {
+            name: "SEASONAL SOUP", 
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAc3StOVEjY0TYgK3sP76b4jDEHfPNxEpPUA&usqp=CAU",
+            desc: "Preparation Changes Daily", 
+            price: "8",
+    }
+},
+    dishes: {
+        dish1: {
+            name: "CORNMEAL CRUSTED CATFISH",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSokSbt7V_cGdPWUiEng5-loUBEKcPeAx61kQ&usqp=CAU",
+            desc: "Salt And Pepper Fries",
+            price: "18 | Add Fried Shrimp + 10",
+        },
+        dish2: {
+            name: "SPICE SEARED SALMON",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq36Jv0JQ5WxCMkCPfiC0q52y7LDoy1zD4jQ&usqp=CAU",
+            desc: "Artichoke, Black Olive, Baby Spinach, Lemon",
+            price: "21",
+        },
+        dish3: {
+            name: "JUMBO LUMP CRAB CAKES",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQAUNfac-vvfDSXf21x1omW-jD2F6vsaak-Q&usqp=CAU",
+            desc: "French Fries, Cole Slaw, Barbecue Aioli",
+            price: "23",
+        },
+        dish4: {
+            name: "SPINACH BAKED ZITI",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSCuYBdrqR095Y73MqxO4A_OTAzNQtEhpwWA&usqp=CAU",
+            desc: "Baby Spinach, Roasted Tomato, Ricotta, Mozzarella (vegetarian)",
+            price: "17",
+        },
+        dish5: {
+            name: "ST. LOUIS SPARE RIBS",
+            url: "https://www.victoriamag.com/wp-content/uploads/2018/09/recipe_shortribs.jpg",
+            desc: "Mashed Potatoes And Collard Greens",
+            price: "21",
+        }
+
+    },
+    desserts: {
+        dessert1: {
+            name:"Peach Cobbler",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjBA40ILvC21F8hBNTvnajpqLUdGXA2AucEw&usqp=CAU",
+            desc: "Good 'ole Peach Cobbler made with peaches from our Peach State Georgia",
+            price: "8",
+        },
+        dessert2: {
+            name:"Cheesecake",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEmKv67alF4j2NmonGIYkM4qb65Ke7KvGjcQ&usqp=CAU",
+            desc: " classic New York-style cheesecake with a buttery graham cracker crust and rich, dense filling ",
+            price: "8",
+        },
+        dessert3: {
+            name:"Chocolate Cake",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_5Ucfmz07LIGDO2zewLfNxpbsscJlgGstHA&usqp=CAU",
+            desc: "A soft and tender Chocolate Fudge Cake made with cocoa power and sour cream, covered in a rich and creamy chocolate buttercream.",
+            price: "8",
+        },
+        dessert4: {
+            name:"Cinnamon Swirl",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdhfeEqzVP5NlSJLVQ32BBtpGDj56i0HBsoA&usqp=CAU",
+            desc: "These fluffy, creamy, soft cinnamon rolls are filled with a cinnamon brown sugar filling and are topped with a delicious cream cheese frosting",
+            price: "8",
+        }
+        
+    }
+}
+
+
+let counterBevs=0;
+let counterStarters=0;
+let counterMains=0;
+let counterDesserts=0;
+
+
+let cocktails = document.querySelector('#cocktails')
+const bevs = ()=>{
+    
+        if (counterBevs ===0){
+    let sec1img = document.querySelector('.sec1img')
+    sec1img.setAttribute('src', menu.bevs.bev1.url)
+    let name = document.querySelector('.s1title')
+    name.innerHTML = menu.bevs.bev1.name
+    let desc = document.querySelector('.s1desc')
+    desc.innerHTML = menu.bevs.bev1.desc
+    let price = document.querySelector('.s1price')
+    price.innerHTML = menu.bevs.bev1.price  
+    counterBevs++;
+        }else if(counterBevs ===1){
+            let sec1img = document.querySelector('.sec1img')
+            sec1img.setAttribute('src', menu.bevs.bev2.url)
+            let name = document.querySelector('.s1title')
+            name.innerHTML = menu.bevs.bev2.name
+            let desc = document.querySelector('.s1desc')
+            desc.innerHTML = menu.bevs.bev2.desc
+            let price = document.querySelector('.s1price')
+            price.innerHTML = menu.bevs.bev2.price  
+            counterBevs++;
+        }else if(counterBevs ===2){
+            let sec1img = document.querySelector('.sec1img')
+            sec1img.setAttribute('src', menu.bevs.bev3.url)
+            let name = document.querySelector('.s1title')
+            name.innerHTML = menu.bevs.bev3.name
+            let desc = document.querySelector('.s1desc')
+            desc.innerHTML = menu.bevs.bev3.desc
+            let price = document.querySelector('.s1price')
+            price.innerHTML = menu.bevs.bev3.price  
+            counterBevs++;
+        }else if(counterBevs ===3){
+            let sec1img = document.querySelector('.sec1img')
+            sec1img.setAttribute('src', menu.bevs.bev4.url)
+            let name = document.querySelector('.s1title')
+            name.innerHTML = menu.bevs.bev4.name
+            let desc = document.querySelector('.s1desc')
+            desc.innerHTML = menu.bevs.bev4.desc
+            let price = document.querySelector('.s1price')
+            price.innerHTML = menu.bevs.bev4.price  
+            counterBevs++;
+        }else if(counterBevs ===4){
+            let sec1img = document.querySelector('.sec1img')
+            sec1img.setAttribute('src', menu.bevs.bev5.url)
+            let name = document.querySelector('.s1title')
+            name.innerHTML = menu.bevs.bev5.name
+            let desc = document.querySelector('.s1desc')
+            desc.innerHTML = menu.bevs.bev5.desc
+            let price = document.querySelector('.s1price')
+            price.innerHTML = menu.bevs.bev5.price  
+            counterBevs++;  
+        }else if(counterBevs ===5){
+            let sec1img = document.querySelector('.sec1img')
+            sec1img.setAttribute('src', menu.bevs.bev1.url)
+            let name = document.querySelector('.s1title')
+            name.innerHTML = menu.bevs.bev1.name
+            let desc = document.querySelector('.s1desc')
+            desc.innerHTML = menu.bevs.bev1.desc
+            let price = document.querySelector('.s1price')
+            price.innerHTML = menu.bevs.bev1.price  
+            counterBevs=0;  
+        };
+        }
+
+
+
+cocktails.addEventListener('click', bevs)
+
+
+    
+
+let starters = document.querySelector('#starters')
+const apps = ()=>{
+    
+        if (counterStarters ===0){
+    let sec2img = document.querySelector('.sec2img')
+    sec2img.setAttribute('src', menu.apps.app1.url)
+    let name = document.querySelector('.s2title')
+    name.innerHTML = menu.apps.app1.name
+    let desc = document.querySelector('.s2desc')
+    desc.innerHTML = menu.apps.app1.desc
+    let price = document.querySelector('.s2price')
+    price.innerHTML = menu.apps.app1.price  
+    counterStarters+=1;
+        }else if(counterStarters ===1){
+                let sec2img = document.querySelector('.sec2img')
+                sec2img.setAttribute('src', menu.apps.app2.url)
+                let name = document.querySelector('.s2title')
+                name.innerHTML = menu.apps.app2.name
+                let desc = document.querySelector('.s2desc')
+                desc.innerHTML = menu.apps.app2.desc
+                let price = document.querySelector('.s2price')
+                price.innerHTML = menu.apps.app2.price  
+                counterStarters+=1;
+            }else if(counterStarters ===2){
+                let sec2img = document.querySelector('.sec2img')
+                sec2img.setAttribute('src', menu.apps.app3.url)
+                let name = document.querySelector('.s2title')
+                name.innerHTML = menu.apps.app3.name
+                let desc = document.querySelector('.s2desc')
+                desc.innerHTML = menu.apps.app3.desc
+                let price = document.querySelector('.s2price')
+                price.innerHTML = menu.apps.app3.price  
+                counterStarters+=1;
+            }else if(counterStarters ===3){
+                let sec2img = document.querySelector('.sec2img')
+                sec2img.setAttribute('src', menu.apps.app4.url)
+                let name = document.querySelector('.s2title')
+                name.innerHTML = menu.apps.app4.name
+                let desc = document.querySelector('.s2desc')
+                desc.innerHTML = menu.apps.app4.desc
+                let price = document.querySelector('.s2price')
+                price.innerHTML = menu.apps.app4.price  
+                counterStarters+=1;
+            }else if(counterStarters ===4){
+                let sec2img = document.querySelector('.sec2img')
+                sec2img.setAttribute('src', menu.apps.app5.url)
+                let name = document.querySelector('.s2title')
+                name.innerHTML = menu.apps.app5.name
+                let desc = document.querySelector('.s2desc')
+                desc.innerHTML = menu.apps.app5.desc
+                let price = document.querySelector('.s2price')
+                price.innerHTML = menu.apps.app5.price  
+                counterStarters+=1;
+            }else if(counterStarters ===5){
+                let sec2img = document.querySelector('.sec2img')
+                sec2img.setAttribute('src', menu.apps.app1.url)
+                let name = document.querySelector('.s2title')
+                name.innerHTML = menu.apps.app1.name
+                let desc = document.querySelector('.s2desc')
+                desc.innerHTML = menu.apps.app1.desc
+                let price = document.querySelector('.s2price')
+                price.innerHTML = menu.apps.app1.price  
+                counterStarters=0;
+    
+    
+    
+}
+}
+
+
+
+starters.addEventListener('click', apps)
+
+let mains = document.querySelector('#mains')
+const dishes = ()=>{
+    
+    if (counterMains ===0){
+    let sec3img = document.querySelector('.sec3img')
+    sec3img.setAttribute('src', menu.dishes.dish1.url)
+    let name = document.querySelector('.s3title')
+    name.innerHTML = menu.dishes.dish1.name
+    let desc = document.querySelector('.s3desc')
+    desc.innerHTML = menu.dishes.dish1.desc
+    let price = document.querySelector('.s3price')
+    price.innerHTML = menu.dishes.dish1.price 
+    counterMains+=1;
+        }else if(counterMains ===1){
+                let sec3img = document.querySelector('.sec3img')
+                sec3img.setAttribute('src', menu.dishes.dish2.url)
+                let name = document.querySelector('.s3title')
+                name.innerHTML = menu.dishes.dish2.name
+                let desc = document.querySelector('.s3desc')
+                desc.innerHTML = menu.dishes.dish2.desc
+                let price = document.querySelector('.s3price')
+                price.innerHTML = menu.dishes.dish2.price  
+                counterMains+=1;
+            }else if(counterMains ===2){
+                let sec3img = document.querySelector('.sec3img')
+                sec3img.setAttribute('src', menu.dishes.dish3.url)
+                let name = document.querySelector('.s3title')
+                name.innerHTML = menu.dishes.dish3.name
+                let desc = document.querySelector('.s3desc')
+                desc.innerHTML = menu.dishes.dish3.desc
+                let price = document.querySelector('.s3price')
+                price.innerHTML = menu.dishes.dish3.price
+                counterMains+=1;
+            }else if(counterMains ===3){
+                let sec3img = document.querySelector('.sec3img')
+                sec3img.setAttribute('src', menu.dishes.dish4.url)
+                let name = document.querySelector('.s3title')
+                name.innerHTML = menu.dishes.dish4.name
+                let desc = document.querySelector('.s3desc')
+                desc.innerHTML = menu.dishes.dish4.desc
+                let price = document.querySelector('.s3price')
+                price.innerHTML = menu.dishes.dish4.price  
+                counterMains+=1;
+            }else if(counterMains ===4){
+                let sec3img = document.querySelector('.sec3img')
+                sec3img.setAttribute('src', menu.dishes.dish5.url)
+                let name = document.querySelector('.s3title')
+                name.innerHTML = menu.dishes.dish5.name
+                let desc = document.querySelector('.s3desc')
+                desc.innerHTML = menu.dishes.dish5.desc
+                let price = document.querySelector('.s3price')
+                price.innerHTML = menu.dishes.dish5.price  
+                counterMains+=1;
+            }else if(counterMains ===5){
+                let sec3img = document.querySelector('.sec3img')
+                sec3img.setAttribute('src', menu.dishes.dish1.url)
+                let name = document.querySelector('.s3title')
+                name.innerHTML = menu.dishes.dish1.name
+                let desc = document.querySelector('.s3desc')
+                desc.innerHTML = menu.dishes.dish1.desc
+                let price = document.querySelector('.s3price')
+                price.innerHTML = menu.dishes.dish1.price  
+                counterMains=0;
+    
+    
+    
+}
+}
+
+
+
+mains.addEventListener('click', dishes)
+
+
+let desserts = document.querySelector('#desserts')
+const Desserts = ()=>{
+    
+    if (counterDesserts ===0){
+    let sec4img = document.querySelector('.sec4img')
+    sec4img.setAttribute('src', menu.desserts.dessert1.url)
+    let name = document.querySelector('.s4title')
+    name.innerHTML = menu.desserts.dessert1.name
+    let desc = document.querySelector('.s4desc')
+    desc.innerHTML = menu.desserts.dessert1.desc
+    let price = document.querySelector('.s4price')
+    price.innerHTML = menu.desserts.dessert1.price  
+    counterDesserts+=1;
+        }else if(counterDesserts ===1){
+                let sec4img = document.querySelector('.sec4img')
+                sec4img.setAttribute('src', menu.desserts.dessert2.url)
+                let name = document.querySelector('.s4title')
+                name.innerHTML = menu.desserts.dessert2.name
+                let desc = document.querySelector('.s4desc')
+                desc.innerHTML = menu.desserts.dessert2.desc
+                let price = document.querySelector('.s4price')
+                price.innerHTML = menu.desserts.dessert2.price  
+                counterDesserts+=1;
+            }else if(counterDesserts ===2){
+                let sec4img = document.querySelector('.sec4img')
+                sec4img.setAttribute('src', menu.desserts.dessert3.url)
+                let name = document.querySelector('.s4title')
+                name.innerHTML = menu.desserts.dessert3.name
+                let desc = document.querySelector('.s4desc')
+                desc.innerHTML = menu.desserts.dessert3.desc
+                let price = document.querySelector('.s4price')
+                price.innerHTML = menu.desserts.dessert3.price
+                counterDesserts+=1;
+            }else if(counterDesserts ===3){
+                let sec4img = document.querySelector('.sec4img')
+                sec4img.setAttribute('src', menu.desserts.dessert4.url)
+                let name = document.querySelector('.s4title')
+                name.innerHTML = menu.desserts.dessert4.name
+                let desc = document.querySelector('.s4desc')
+                desc.innerHTML = menu.desserts.dessert4.desc
+                let price = document.querySelector('.s4price')
+                price.innerHTML = menu.desserts.dessert4.price  
+                counterDesserts+=1;
+            }else if(counterDesserts ===4){
+                let sec4img = document.querySelector('.sec4img')
+                sec4img.setAttribute('src', menu.desserts.dessert1.url)
+                let name = document.querySelector('.s4title')
+                name.innerHTML = menu.desserts.dessert1.name
+                let desc = document.querySelector('.s4desc')
+                desc.innerHTML = menu.desserts.dessert1.desc
+                let price = document.querySelector('.s4price')
+                price.innerHTML = menu.desserts.dessert1.price  
+                counterDesserts=0;
+    
+    
+    
+}
+}
+
+
+
+desserts.addEventListener('click', Desserts)
